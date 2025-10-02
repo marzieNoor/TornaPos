c78e1427b99dda3aa65fd2dc174cce2746970b75
marzi <marzieh.nourmohammadi@gmail.com>
2025-10-02 15:25:45 +0330

    add jpos dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    packaging {
+    packaging  {
         resources {
             excludes += arrayOf(
                 "META-INF/DEPENDENCIES",
@@ -29,6 +29,7 @@ android {
             )
         }
     }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -58,10 +59,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
 
     implementation(libs.jpos)
+    implementation(libs.datastorePreference)
 }
